84e04a8a0d8bfabeadde8b1c7451cadcbb86462a
Munawar Hussain Shakil <shakil.ruet.12@gmail.com>
2019-06-04 23:18:04 +0600

    Add files via upload
    Commit build files

---
+++ precache-manifest.1142ef6e8ed0e0adc8889ef9e218a95d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480e1d34034adac00ce869d65d065a83",
+    "url": "/mini-slate/index.html"
+  },
+  {
+    "revision": "08886f5f8eaf9ad884aa",
+    "url": "/mini-slate/static/css/main.f217d2e1.chunk.css"
+  },
+  {
+    "revision": "0ee91edca8b40822266b",
+    "url": "/mini-slate/static/js/2.9ca3d95f.chunk.js"
+  },
+  {
+    "revision": "08886f5f8eaf9ad884aa",
+    "url": "/mini-slate/static/js/main.f0fb664e.chunk.js"
+  },
+  {
+    "revision": "15f6c79cd45a7b772804",
+    "url": "/mini-slate/static/js/runtime~main.0e8be4d7.js"
+  }
+]);
